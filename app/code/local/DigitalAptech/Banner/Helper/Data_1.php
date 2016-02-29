7a291470057e471e7fc3db4964428441fa5fee44
dipanwita <dipanwita@digitalaptech.com>
2016-02-29 20:49:01 +0530

    details page design is done and banner module is added

--- app/code/local/DigitalAptech/Banner/Helper/Data_1.php
+++
@@ -1,5 +0,0 @@
-<?php
-class DigitalAptech_Banner_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
--- app/code/local/DigitalAptech/Banner/Model/Mysql4/Banner_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-class DigitalAptech_Banner_Model_Mysql4_Banner extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init("banner/banner", "banner_id");
-    }
-}
