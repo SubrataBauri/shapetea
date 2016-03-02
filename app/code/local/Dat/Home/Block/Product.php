bad2bac27147ccc99825b1de2a0ae1697666f692
dipanwita <dipanwita@digitalaptech.com>
2016-03-02 20:43:39 +0530

    review and rating section added in homepage  +reviews.phtml is the phtml to shhow in frontend  +Product.php is the block review

--- app/code/local/Dat/Home/Block/Product.php
+++ app/code/local/Dat/Home/Block/Product.php
@@ -23,4 +23,36 @@ class Dat_Home_Block_Product extends Mage_Core_Block_Template {
         return $products;
     }
 
+//    function getReviews() {
+//
+//        $reviews = Mage::getModel('review/review')->getResourceCollection();
+//
+//        $reviews->addStoreFilter(Mage::app()->getStore()->getId())
+//                ->addStatusFilter(Mage_Review_Model_Review::STATUS_APPROVED)
+//                ->setDateOrder()
+//                ->addRateVotes()
+//                ->load();
+//        return $reviews;
+//    }
+    public function getReviewsCollection() {
+
+        $reviewscollection = Mage::getModel('review/review')->getCollection();
+        $reviewscollection->getSelect()->joinLeft(array('rating' => Mage::getSingleton("core/resource")
+                    ->getTableName('rating_option_vote')), 'main_table.review_id = rating.review_id', 
+                        array('rating.percent'));
+        $reviewscollection->getSelect()->order('rating.percent DESC')
+                                        ->limit(3);
+        //Mage::log($reviewscollection->getSelectSql(true));
+        return $reviewscollection;
+    }
+
+    public function getRating($review_id) {
+        $rating = Mage::getModel('rating/rating_option_vote')
+                ->getResourceCollection()
+                ->addFieldToFilter('review_id', $review_id);
+
+
+        return $rating;
+    }
+
 }
